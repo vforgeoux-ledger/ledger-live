e31bc30166797691804d3a7cf4f5deeeb1c27e9f
male-gal <maxence.le-gall@ledger.fr>
2024-02-29 09:42:11 +0100

    UI WIP

--- apps/web-tools/app/layout.tsx
+++ apps/web-tools/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -21,7 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- apps/web-tools/app/portfolio/page.tsx
+++ apps/web-tools/app/portfolio/page.tsx
@@ -2,6 +2,7 @@
 import { AssetItem } from "@/components/system/AssetItem";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Ledger from "@/components/ui/icons/ledger";
 import Link from "next/link";
 
 const assets = [
@@ -26,30 +27,45 @@ const usePortfolio = () => {
     console.log("send");
   };
 
-  return { handleAddClick, handleSendClick };
+  const balance = 800;
+
+  return { handleAddClick, handleSendClick, balance };
 };
 
 function Portfolio() {
-  const { handleAddClick, handleSendClick } = usePortfolio();
+  const { handleAddClick, handleSendClick, balance } = usePortfolio();
 
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <Link href="/portfolio-debugger">Portfolio Debugger</Link>
-      <Card className="w-content">
-        <CardHeader className="flex flex-row">
-          <CardTitle>My assets</CardTitle>
-          <div className="flex flex-row gap-4">
-            <Button onClick={handleAddClick}>Add asset</Button>
-            <Button onClick={handleSendClick}>Send</Button>
-          </div>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-3">
-          {assets.map(asset => (
-            <AssetItem asset={asset} key={asset.id} />
-          ))}
-        </CardContent>
-      </Card>
+    <div className="flex w-full h-screen justify-center items-center">
+      <div className="flex flex-col gap-4 w-content">
+        <div className="flex flex-row gap-4">
+          <Ledger className="h-8" />
+          <h1>Portfolio</h1>
+        </div>
+        <Card className="w-content">
+          <CardHeader className="flex flex-row justify-between">
+            <CardTitle>Balance</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-3">
+            <h4>{balance}</h4>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row justify-between">
+            <CardTitle>My assets</CardTitle>
+            <div className="flex flex-row gap-4">
+              <Button onClick={handleAddClick}>Add asset</Button>
+              <Button onClick={handleSendClick}>Send</Button>
+            </div>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-3">
+            {assets.map(asset => (
+              <AssetItem asset={asset} key={asset.id} />
+            ))}
+          </CardContent>
+        </Card>
+        <Link href="/portfolio-debugger">Portfolio Debugger</Link>
+      </div>
     </div>
   );
 }
