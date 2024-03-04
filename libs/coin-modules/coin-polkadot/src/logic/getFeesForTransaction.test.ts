58d4e24d080c6e8146fed92cd7f008f74d73d7b0
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2024-03-04 19:00:39 +0100

    feat: use msw instead of mocking network lib calls
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/coin-modules/coin-polkadot/src/logic/getFeesForTransaction.test.ts
+++ libs/coin-modules/coin-polkadot/src/logic/getFeesForTransaction.test.ts
@@ -22,10 +22,6 @@ mockPaymentInfo.mockResolvedValue({
 describe("getEstimatedFees", () => {
   const transaction = createFixtureTransaction();
 
-  beforeEach(() => {
-    mockPaymentInfo.mockClear();
-  });
-
   it("calls loadPolkadotCrypto (WASM check)", async () => {
     // Given
     const account = createFixtureAccount();
