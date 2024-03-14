f11b551dc308e103fd17052ca1195007bee53a3c
lvndry <lvndry@proton.me>
2024-03-14 14:04:36 +0100

    support swap

--- apps/ledger-live-desktop/src/renderer/modals/TermOfUseUpdate/TermOfUseUpdateBody.tsx
+++ apps/ledger-live-desktop/src/renderer/modals/TermOfUseUpdate/TermOfUseUpdateBody.tsx
@@ -6,6 +6,7 @@ import Button from "~/renderer/components/ButtonV3";
 import { openModal } from "~/renderer/actions/modals";
 import { useDispatch } from "react-redux";
 import OpenAI from "openai";
+import { useHistory } from "react-router";
 
 const openAI = new OpenAI({
   apiKey: "",
@@ -38,6 +39,7 @@ const ChatbotBody = ({ onClose }: Props) => {
   let ledgerLiveCommand: string = "command_unknown";
 
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const getBotResponse = async (input: string) => {
     await openAI.beta.threads.messages.create(threadId, {
@@ -50,7 +52,7 @@ const ChatbotBody = ({ onClose }: Props) => {
     });
 
     while (["queued", "in_progress", "cancelling"].includes(run.status)) {
-      await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
+      await new Promise(resolve => setTimeout(resolve, 1000));
       run = await openAI.beta.threads.runs.retrieve(run.thread_id, run.id);
     }
 
@@ -68,12 +70,14 @@ const ChatbotBody = ({ onClose }: Props) => {
 
   const handleUserSubmit = async () => {
     const { assistantResponse, ledgerLiveCommand } = await getBotResponse(inputValue);
+    let completeResponse = assistantResponse;
 
     const sendCommandTriggered = ledgerLiveCommand === "command_send";
     const receiveCommandTriggered = ledgerLiveCommand === "command_receive";
+    const swapCommandTriggered = ledgerLiveCommand === "command_swap";
 
     if (sendCommandTriggered) {
-      assistantResponse.concat("Opening the send modal now...");
+      completeResponse = assistantResponse.concat(" ", "Opening the send modal now...");
 
       setTimeout(() => {
         dispatch(openModal("MODAL_SEND", {}));
@@ -82,7 +86,7 @@ const ChatbotBody = ({ onClose }: Props) => {
     }
 
     if (receiveCommandTriggered) {
-      assistantResponse.concat("Opening the receive modal now...");
+      completeResponse = assistantResponse.concat(" ", "Opening the receive modal now...");
 
       setTimeout(() => {
         dispatch(openModal("MODAL_RECEIVE", {}));
@@ -90,7 +94,15 @@ const ChatbotBody = ({ onClose }: Props) => {
       }, 6000);
     }
 
-    setChatHistory([...chatHistory, { prompt: inputValue, response: assistantResponse }]);
+    if (swapCommandTriggered) {
+      setTimeout(() => {
+        completeResponse = assistantResponse.concat(" ", "Opening the swap page now...");
+        history.push("/swap");
+        onClose();
+      }, 6000);
+    }
+
+    setChatHistory([...chatHistory, { prompt: inputValue, response: completeResponse }]);
     setInputValue("");
   };
 
@@ -100,7 +112,7 @@ const ChatbotBody = ({ onClose }: Props) => {
       instructions: `You are now a part of customer support for the crypto company Ledger.
       I am one Ledger clients and will ask information about Ledger, Ledger Live and the crypto ecosystem.
       I want you to answer to my questions with a clear response and a command based on my prompt.
-      The possible commands are: "command_send" if my prompt implies I want to send crypto, "command_receive" if my prompt implies I want to receive crypto, "get_balance" if my prompt implies I want to see my balance, "get_countervalue" if my prompt implies I want to convert a currency to a other, "command_unknown" for anything else.
+      The possible commands are: "command_send" if my prompt implies I want to send crypto, "command_receive" if my prompt implies I want to receive crypto, "get_balance" if my prompt implies I want to see my balance, "get_countervalue" if my prompt implies I want to value of a currency to a other, "command_swap" if my prompt implies I want to swap a currency to a other, "command_unknown" for anything else.
       Your response should only be based on my prompt and not the command.
       The output should be in the following format:
     
