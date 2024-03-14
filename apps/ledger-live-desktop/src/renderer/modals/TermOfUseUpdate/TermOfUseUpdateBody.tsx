fd5710819a45e3cfb0c077e3529e5db282905174
lvndry <lvndry@proton.me>
2024-03-14 13:30:09 +0100

    use openai assistant

--- apps/ledger-live-desktop/src/renderer/modals/TermOfUseUpdate/TermOfUseUpdateBody.tsx
+++ apps/ledger-live-desktop/src/renderer/modals/TermOfUseUpdate/TermOfUseUpdateBody.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Flex, Text } from "@ledgerhq/react-ui";
 import styled from "styled-components";
 import { ModalBody } from "~/renderer/components/Modal";
 import Button from "~/renderer/components/ButtonV3";
-import axios from "axios";
 import { openModal } from "~/renderer/actions/modals";
 import { useDispatch } from "react-redux";
-import { send } from "xstate/lib/actions";
+import OpenAI from "openai";
+
+const openAI = new OpenAI({
+  apiKey: "",
+  dangerouslyAllowBrowser: true,
+});
 
 type Props = {
   onClose: () => void;
 };
-
-const Updates = styled.ul`
-  margin-left: 16px;
-`;
 
 const BodyText = styled(Text).attrs(() => ({
   variant: "paragraph",
@@ -24,10 +24,6 @@ const BodyText = styled(Text).attrs(() => ({
   color: ${p => p.theme.colors.neutral.c70};
 `;
 
-const Update = styled(BodyText).attrs(() => ({
-  as: "li",
-}))``;
-
 type ChatHistory = {
   prompt: string;
   response: string;
@@ -36,57 +32,94 @@ type ChatHistory = {
 const ChatbotBody = ({ onClose }: Props) => {
   const [inputValue, setInputValue] = useState("");
   const [chatHistory, setChatHistory] = useState<ChatHistory[]>([]);
+  const [threadId, setThreadId] = useState("");
+  const [assistantId, setAssistantId] = useState("");
+  let assistantResponse = "";
+  let ledgerLiveCommand: string = "command_unknown";
+
   const dispatch = useDispatch();
 
   const getBotResponse = async (input: string) => {
-    const commands = ["receive crypto", "send crypto", "swap cryto", "None of above"];
+    await openAI.beta.threads.messages.create(threadId, {
+      role: "user",
+      content: input,
+    });
 
-    const response = await axios.post<{ choices: { message: { content: string } }[] }>(
-      "https://api.openai.com/v1/chat/completions",
-      {
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: input }],
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer`,
-        },
-      },
-    );
+    let run = await openAI.beta.threads.runs.create(threadId, {
+      assistant_id: assistantId,
+    });
 
-    console.log(response.data.choices[0].message.content);
+    while (["queued", "in_progress", "cancelling"].includes(run.status)) {
+      await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
+      run = await openAI.beta.threads.runs.retrieve(run.thread_id, run.id);
+    }
 
-    return response.data.choices[0].message.content;
+    if (run.status === "completed") {
+      const messages = await openAI.beta.threads.messages.list(run.thread_id);
+      const [, command, response] = messages.data[0].content[0].text.value.split("\n");
+      assistantResponse = response.split("response:")[1].trim();
+      ledgerLiveCommand = command.split("command:")[1].trim();
+    }
+
+    console.log({ assistantResponse, ledgerLiveCommand });
+
+    return { assistantResponse, ledgerLiveCommand };
   };
 
   const handleUserSubmit = async () => {
-    const botResponse = await getBotResponse(inputValue);
+    const { assistantResponse, ledgerLiveCommand } = await getBotResponse(inputValue);
 
-    const sendCommandTriggered = inputValue.includes("send");
-    const receiveCommandTriggered = inputValue.includes("receive");
+    const sendCommandTriggered = ledgerLiveCommand === "command_send";
+    const receiveCommandTriggered = ledgerLiveCommand === "command_receive";
 
     if (sendCommandTriggered) {
-      botResponse.concat("Opening the send modal now...");
+      assistantResponse.concat("Opening the send modal now...");
 
       setTimeout(() => {
         dispatch(openModal("MODAL_SEND", {}));
         onClose();
-      }, 10000);
+      }, 6000);
     }
 
     if (receiveCommandTriggered) {
-      botResponse.concat("Opening the receive modal now...");
+      assistantResponse.concat("Opening the receive modal now...");
 
       setTimeout(() => {
         dispatch(openModal("MODAL_RECEIVE", {}));
         onClose();
-      }, 10000);
+      }, 6000);
     }
 
-    setChatHistory([...chatHistory, { prompt: inputValue, response: botResponse }]);
+    setChatHistory([...chatHistory, { prompt: inputValue, response: assistantResponse }]);
     setInputValue("");
   };
+
+  const setupAssistant = async () => {
+    const assistant = await openAI.beta.assistants.create({
+      name: "Ledger Live",
+      instructions: `You are now a part of customer support for the crypto company Ledger.
+      I am one Ledger clients and will ask information about Ledger, Ledger Live and the crypto ecosystem.
+      I want you to answer to my questions with a clear response and a command based on my prompt.
+      The possible commands are: "command_send" if my prompt implies I want to send crypto, "command_receive" if my prompt implies I want to receive crypto, "get_balance" if my prompt implies I want to see my balance, "get_countervalue" if my prompt implies I want to convert a currency to a other, "command_unknown" for anything else.
+      Your response should only be based on my prompt and not the command.
+      The output should be in the following format:
+    
+      """
+      command: <command>
+      response: <response>
+      """`,
+      model: "gpt-3.5-turbo",
+    });
+
+    const thread = await openAI.beta.threads.create();
+
+    setAssistantId(assistant.id);
+    setThreadId(thread.id);
+  };
+
+  useEffect(() => {
+    setupAssistant();
+  }, []);
 
   return (
     <ModalBody
