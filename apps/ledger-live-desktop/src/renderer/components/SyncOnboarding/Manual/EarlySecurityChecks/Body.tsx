e571ac387c1be326495792c1395487cf1faeed4f
Kevin Le Seigle <kevin.le-seigle@ledger.fr>
2024-04-03 12:01:25 +0200

    fix: linter with new version

--- apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/Manual/EarlySecurityChecks/Body.tsx
+++ apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/Manual/EarlySecurityChecks/Body.tsx
@@ -67,23 +67,23 @@ const SoftwareCheckContent = ({
                 modelName,
               })
             : genuineCheckStatus === SoftwareCheckStatus.cancelled
-            ? t("syncOnboarding.manual.softwareCheckContent.genuineCheck.title.cancelled", {
-                modelName,
-              })
-            : t("syncOnboarding.manual.softwareCheckContent.genuineCheck.title.active", {
-                modelName,
-              })
+              ? t("syncOnboarding.manual.softwareCheckContent.genuineCheck.title.cancelled", {
+                  modelName,
+                })
+              : t("syncOnboarding.manual.softwareCheckContent.genuineCheck.title.active", {
+                  modelName,
+                })
         }
         subtitle={
           genuineCheckStatus === SoftwareCheckStatus.completed
             ? undefined
             : genuineCheckStatus === SoftwareCheckStatus.cancelled
-            ? t("syncOnboarding.manual.softwareCheckContent.genuineCheck.subtitle.cancelled", {
-                modelName,
-              })
-            : t("syncOnboarding.manual.softwareCheckContent.genuineCheck.subtitle.active", {
-                modelName,
-              })
+              ? t("syncOnboarding.manual.softwareCheckContent.genuineCheck.subtitle.cancelled", {
+                  modelName,
+                })
+              : t("syncOnboarding.manual.softwareCheckContent.genuineCheck.subtitle.active", {
+                  modelName,
+                })
         }
       >
         {genuineCheckStatus === SoftwareCheckStatus.cancelled ? (
@@ -107,13 +107,16 @@ const SoftwareCheckContent = ({
                 modelName,
               })
             : firmwareUpdateStatus === SoftwareCheckStatus.updateAvailable
-            ? t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.title.updateAvailable", {
-                modelName,
-                firmwareVersion: availableFirmwareVersion,
-              })
-            : t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.title.active", {
-                modelName,
-              })
+              ? t(
+                  "syncOnboarding.manual.softwareCheckContent.firmwareUpdate.title.updateAvailable",
+                  {
+                    modelName,
+                    firmwareVersion: availableFirmwareVersion,
+                  },
+                )
+              : t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.title.active", {
+                  modelName,
+                })
         }
         subtitle={
           firmwareUpdateStatus === SoftwareCheckStatus.completed
@@ -121,15 +124,15 @@ const SoftwareCheckContent = ({
                 modelName,
               })
             : firmwareUpdateStatus === SoftwareCheckStatus.updateAvailable
-            ? t(
-                "syncOnboarding.manual.softwareCheckContent.firmwareUpdate.subtitle.updateAvailable",
-                {
+              ? t(
+                  "syncOnboarding.manual.softwareCheckContent.firmwareUpdate.subtitle.updateAvailable",
+                  {
+                    modelName,
+                  },
+                )
+              : t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.subtitle.active", {
                   modelName,
-                },
-              )
-            : t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.subtitle.active", {
-                modelName,
-              })
+                })
         }
       >
         {firmwareUpdateStatus === SoftwareCheckStatus.updateAvailable ? (
